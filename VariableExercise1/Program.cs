e71f1fa9645c595b9dbf7eebe355de57c126cd43
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-02-16 07:47:01 -0800

    made some variables

---
+++ VariableExercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace VariableExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize variables
+            string name = "Muza";
+            int age = 25;
+            char middleInitial = 'A';
+            bool isOver18 = true;
+            double currentTemp = 67.9;
+            decimal currentPrice = 49.99m;
+
+            //Writing te variables to the console
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"age: {age}");
+            Console.WriteLine($"middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+
+
+
+        }
+    }
+}
